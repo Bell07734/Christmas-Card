317f674ecfc1d300ae1d23165ff72491d2a9a886
Bell07734 <oliver_m_bell@yahoo.com>
2022-11-27 07:04:15 +0000

    Added snowy background

---
+++ background.js
@@ -0,0 +1,43 @@
+new p5(function (p5) {
+	let snowflakes = [];
+	p5.setup = function () {
+		bg = p5.createCanvas(p5.windowWidth, p5.windowHeight);
+		bg.parent("bg");
+	};
+
+	p5.draw = function () {
+		p5.background("#091540");
+		p5.noStroke();
+
+		let t = p5.frameCount / 60;
+		for (let i = 0; i < p5.random(3); i++) {
+			snowflakes.push(new Snowflake());
+		}
+		for (snowflake of snowflakes) {
+			snowflake.update(t);
+		}
+	};
+
+	function Snowflake() {
+		this.posX = 0;
+		this.posY = p5.random(-50, 0);
+		this.initialangle = p5.random(0, 2 * p5.PI);
+		this.size = p5.random(2, 5);
+
+		this.radius = p5.sqrt(p5.random(p5.pow(p5.width / 2, 2)));
+
+		this.update = function (time) {
+			let w = 0.6;
+			let angle = w * time + this.initialangle;
+			this.posX = p5.width / 2 + this.radius * p5.sin(angle);
+
+			this.posY += p5.pow(this.size, 0.5);
+
+			if (this.posY > p5.height) {
+				let index = snowflakes.indexOf(this);
+				snowflakes.splice(index, 1);
+			}
+			p5.ellipse(this.posX, this.posY, this.size);
+		};
+	}
+});
--- front.js
+++ front.js
@@ -4,201 +4,251 @@ let currentLocation;
 let music;
 let counter = 0.0;
 let lights;
+let baubles;
 
-function preload() {
-	music = loadSound("WeWishYouAMerryXmas.mp3");
-}
+new p5(function (p5) {
+	p5.preload = function () {
+		music = p5.loadSound("WeWishYouAMerryXmas.mp3");
+	};
 
-function setup() {
-	front = createCanvas(350, 500);
-	front.parent("f1");
+	p5.setup = function () {
+		front = p5.createCanvas(350, 500);
+		front.parent("f1");
 
-	if (windowHeight > windowWidth) {
-		document.getElementById("credits").style.display = "block";
-	}
-
-	card = document.querySelector("#card");
-	card.addEventListener("mousedown", cardClicked);
-	card.addEventListener("touch", cardClicked);
-
-	paper1 = document.querySelector("#p1");
-	paper2 = document.querySelector("#p2");
-
-	currentLocation = 1;
-
-	lightLocations = [
-		[width / 2, height / 2 - 85],
-		[width / 2 + 20, height / 2 - 95],
-		[width / 2 - 30, height / 2],
-		[width / 2 + 25, height / 2 + 10],
-		[width / 2 + 70, height / 2 + 60],
-		[width / 2 - 25, height / 2 + 50],
-		[width / 2 + 10, height / 2 - 25],
-		[width / 2 + 40, height / 2 + 90],
-		[width / 2 - 90, height / 2 + 160],
-		[width / 2 + 65, height / 2 + 155],
-		[width / 2 - 60, height / 2 + 195],
-		[width / 2 - 10, height / 2 + 115],
-		[width / 2 + 35, height / 2 + 175],
-	];
-
-	baubleLocations = [
-		[width / 2 - 20, height / 2 - 80],
-		[width / 2 + 10, height / 2 - 110],
-		[width / 2 - 50, height / 2],
-		[width / 2 + 25, height / 2 + 50],
-		[width / 2 + 30, height / 2 - 10],
-		[width / 2 - 50, height / 2 + 45],
-		[width / 2, height / 2 - 45],
-		[width / 2 + 10, height / 2 + 90],
-		[width / 2 - 75, height / 2 + 150],
-		[width / 2 + 55, height / 2 + 135],
-		[width / 2 - 35, height / 2 + 190],
-		[width / 2 - 30, height / 2 + 105],
-		[width / 2 + 15, height / 2 + 175],
-	];
-
-	baubles = [];
-
-	for (let location of baubleLocations) {
-		baubles.push(new Bauble(location[0], location[1]));
-	}
-
-	lights = [];
-
-	for (let location of lightLocations) {
-		lights.push(new Light(location[0], location[1]));
-	}
-}
-
-function draw() {
-	counter += deltaTime;
-	if (counter > 1000) {
-		counter = 0;
-	}
-
-	background(255);
-	noStroke();
-	tree();
-	messsage();
-
-	updateBaubles();
-	updateLights();
-}
-
-function updateBaubles() {
-	for (let bauble of baubles) {
-		bauble.draw();
-	}
-}
-
-function updateLights() {
-	for (let i = 0; i < lights.length; i++) {
-		light = lights[i];
-		if (i % 2 == floor(counter / 500) % 2) {
-			light.on();
-		} else {
-			light.off();
+		if (p5.windowHeight > p5.windowWidth) {
+			document.getElementById("credits").style.display = "block";
 		}
-		light.draw();
-	}
-}
 
-function cardClicked() {
-	for (let bauble of baubles) {
-		if (bauble.touchingMouse()) {
-			bauble.changeColour();
-			return;
-		}
-	}
-	if (currentLocation == 1) {
-		openCard();
-		paper1.classList.add("flipped");
-		paper1.style.zindex = 1;
-		currentLocation = 2;
-		if (!music.isPlaying()) {
-			music.play();
-		}
-	} else if (currentLocation == 2) {
-		closeCard();
-		paper1.classList.remove("flipped");
-		paper1.style.zindex = 2;
+		card = document.querySelector("#card");
+		card.addEventListener("mousedown", cardClicked);
+		card.addEventListener("touch", cardClicked);
+
+		paper1 = document.querySelector("#p1");
+		paper2 = document.querySelector("#p2");
+
 		currentLocation = 1;
-		if (music.isPlaying()) {
-			music.stop();
+
+		lightLocations = [
+			[p5.width / 2, p5.height / 2 - 85],
+			[p5.width / 2 + 20, p5.height / 2 - 95],
+			[p5.width / 2 - 30, p5.height / 2],
+			[p5.width / 2 + 25, p5.height / 2 + 10],
+			[p5.width / 2 + 70, p5.height / 2 + 60],
+			[p5.width / 2 - 25, p5.height / 2 + 50],
+			[p5.width / 2 + 10, p5.height / 2 - 25],
+			[p5.width / 2 + 40, p5.height / 2 + 90],
+			[p5.width / 2 - 90, p5.height / 2 + 160],
+			[p5.width / 2 + 65, p5.height / 2 + 155],
+			[p5.width / 2 - 60, p5.height / 2 + 195],
+			[p5.width / 2 - 10, p5.height / 2 + 115],
+			[p5.width / 2 + 35, p5.height / 2 + 175],
+		];
+
+		baubleLocations = [
+			[p5.width / 2 - 20, p5.height / 2 - 80],
+			[p5.width / 2 + 10, p5.height / 2 - 110],
+			[p5.width / 2 - 50, p5.height / 2],
+			[p5.width / 2 + 25, p5.height / 2 + 50],
+			[p5.width / 2 + 30, p5.height / 2 - 10],
+			[p5.width / 2 - 50, p5.height / 2 + 45],
+			[p5.width / 2, p5.height / 2 - 45],
+			[p5.width / 2 + 10, p5.height / 2 + 90],
+			[p5.width / 2 - 75, p5.height / 2 + 150],
+			[p5.width / 2 + 55, p5.height / 2 + 135],
+			[p5.width / 2 - 35, p5.height / 2 + 190],
+			[p5.width / 2 - 30, p5.height / 2 + 105],
+			[p5.width / 2 + 15, p5.height / 2 + 175],
+		];
+
+		baubles = [];
+
+		for (let location of baubleLocations) {
+			baubles.push(new Bauble(location[0], location[1]));
+		}
+
+		lights = [];
+
+		for (let location of lightLocations) {
+			lights.push(new Light(location[0], location[1]));
+		}
+	};
+
+	p5.draw = function () {
+		counter += p5.deltaTime;
+		if (counter > 1000) {
+			counter = 0;
+		}
+
+		p5.background(255);
+		p5.noStroke();
+		tree();
+		messsage();
+
+		updateBaubles();
+		updateLights();
+	};
+
+	function updateBaubles() {
+		for (let bauble of baubles) {
+			bauble.draw();
 		}
 	}
-}
 
-function openCard() {
-	if (windowWidth >= 700) {
-		card.style.transform = "translateX(50%)";
-	} else {
-		card.style.transform = "translateX(5%)";
+	function updateLights() {
+		for (let i = 0; i < lights.length; i++) {
+			light = lights[i];
+			if (i % 2 == p5.floor(counter / 500) % 2) {
+				light.on();
+			} else {
+				light.off();
+			}
+			light.draw();
+		}
 	}
-}
 
-function closeCard() {
-	card.style.transform = "translateX(0%)";
-}
-
-function tree() {
-	fill("#6D3D14");
-	rect(width / 2 - 25, height / 2 + 200, 50, 50);
-
-	fill("#95E06C");
-
-	triangle(
-		width / 2 - 50,
-		height / 2 - 50,
-		width / 2,
-		height / 2 - 150,
-		width / 2 + 50,
-		height / 2 - 50
-	);
-	triangle(
-		width / 2 - 100,
-		height / 2 + 75,
-		width / 2,
-		height / 2 - 100,
-		width / 2 + 100,
-		height / 2 + 75
-	);
-	triangle(
-		width / 2 - 130,
-		height / 2 + 200,
-		width / 2,
-		height / 2,
-		width / 2 + 130,
-		height / 2 + 200
-	);
-
-	//STAR
-	fill("#ffe81d");
-	star(width / 2, height / 2 - 150, 15, 40);
-}
-
-function messsage() {
-	fill(240);
-	textSize(40);
-	textFont("Patrick Hand");
-
-	textAlign(RIGHT, TOP);
-	text("Click to", 325, 25);
-	text("open", 325, 60);
-}
-
-function star(x, y, radius1, radius2) {
-	let angle = TWO_PI / 5.0;
-	let halfAngle = angle / 2.0;
-	beginShape();
-	for (let a = 60; a < TWO_PI + 60; a += angle) {
-		let sx = x + cos(a) * radius2;
-		let sy = y + sin(a) * radius2;
-		vertex(sx, sy);
-		sx = x + cos(a + halfAngle) * radius1;
-		sy = y + sin(a + halfAngle) * radius1;
-		vertex(sx, sy);
+	function cardClicked() {
+		for (let bauble of baubles) {
+			if (bauble.touchingMouse()) {
+				bauble.changeColour();
+				return;
+			}
+		}
+		if (currentLocation == 1) {
+			openCard();
+			paper1.classList.add("flipped");
+			paper1.style.zindex = 1;
+			currentLocation = 2;
+			if (!music.isPlaying()) {
+				music.play();
+			}
+		} else if (currentLocation == 2) {
+			closeCard();
+			paper1.classList.remove("flipped");
+			paper1.style.zindex = 2;
+			currentLocation = 1;
+			if (music.isPlaying()) {
+				music.stop();
+			}
+		}
 	}
-	endShape(CLOSE);
-}
+
+	function openCard() {
+		if (p5.windowWidth >= 700) {
+			card.style.transform = "translateX(50%)";
+		} else {
+			card.style.transform = "translateX(5%)";
+		}
+	}
+
+	function closeCard() {
+		card.style.transform = "translateX(0%)";
+	}
+
+	function tree() {
+		p5.fill("#6D3D14");
+		p5.rect(p5.width / 2 - 25, p5.height / 2 + 200, 50, 50);
+
+		p5.fill("#95E06C");
+
+		p5.triangle(
+			p5.width / 2 - 50,
+			p5.height / 2 - 50,
+			p5.width / 2,
+			p5.height / 2 - 150,
+			p5.width / 2 + 50,
+			p5.height / 2 - 50
+		);
+		p5.triangle(
+			p5.width / 2 - 100,
+			p5.height / 2 + 75,
+			p5.width / 2,
+			p5.height / 2 - 100,
+			p5.width / 2 + 100,
+			p5.height / 2 + 75
+		);
+		p5.triangle(
+			p5.width / 2 - 130,
+			p5.height / 2 + 200,
+			p5.width / 2,
+			p5.height / 2,
+			p5.width / 2 + 130,
+			p5.height / 2 + 200
+		);
+
+		//STAR
+		p5.fill("#ffe81d");
+		star(p5.width / 2, p5.height / 2 - 150, 15, 40);
+	}
+
+	function messsage() {
+		p5.fill(240);
+		p5.textSize(40);
+		p5.textFont("Patrick Hand");
+
+		p5.textAlign(p5.RIGHT, p5.TOP);
+		p5.text("Click to", 325, 25);
+		p5.text("open", 325, 60);
+	}
+
+	function star(x, y, radius1, radius2) {
+		let angle = p5.TWO_PI / 5.0;
+		let halfAngle = angle / 2.0;
+		p5.beginShape();
+		for (let a = 60; a < p5.TWO_PI + 60; a += angle) {
+			let sx = x + p5.cos(a) * radius2;
+			let sy = y + p5.sin(a) * radius2;
+			p5.vertex(sx, sy);
+			sx = x + p5.cos(a + halfAngle) * radius1;
+			sy = y + p5.sin(a + halfAngle) * radius1;
+			p5.vertex(sx, sy);
+		}
+		p5.endShape(p5.CLOSE);
+	}
+	class Bauble {
+		constructor(x, y) {
+			this.colours = [
+				"#FFBC42",
+				"#F95738",
+				"#30BCED",
+				"#88498F",
+				"#FFBC42",
+				"#F95738",
+				"#30BCED",
+				"#88498F",
+			];
+			this.colour = p5.random(this.colours);
+			this.x = x;
+			this.y = y;
+			this.isPressed = false;
+		}
+		draw() {
+			p5.fill(this.colour);
+			p5.circle(this.x, this.y, 25);
+		}
+		touchingMouse() {
+			if (p5.dist(this.x, this.y, p5.mouseX, p5.mouseY) <= 25) {
+				return true;
+			}
+		}
+		changeColour() {
+			this.colour = this.colours[this.colours.indexOf(this.colour) + 1];
+		}
+	}
+	class Light {
+		constructor(x, y) {
+			this.x = x;
+			this.y = y;
+			this.colour = "#ffe81d6e";
+		}
+		draw() {
+			p5.fill(this.colour);
+			p5.circle(this.x, this.y, 10);
+		}
+		on() {
+			this.colour = "#ffe81d";
+		}
+		off() {
+			this.colour = "#ffe81d6e";
+		}
+	}
+});
